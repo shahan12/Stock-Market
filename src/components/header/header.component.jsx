a9d68c02cfd8b2ecd6607a5196e5ed059bebb1bf
Shahan Khan <shahankhan485@gmail.com>
2023-01-07 20:10:09 +0530

    create a sticky header component and create routes

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import classes from "../header/header.module.css";
+import Logo from "../../assets/images/Logo.png";
+import { Link } from "react-router-dom";
 function Header() {
-  return <article className={classes.container}></article>;
+  return (
+    <article className={classes.container}>
+      <header className={classes.wrapper}>
+        <Link to={"/instruments"} className={classes.imgWrapper}>
+          <img src={Logo} className={classes.logo}></img>
+          <span>SENSIBULL</span>
+        </Link>
+      </header>
+    </article>
+  );
 }
 
 export default Header;
