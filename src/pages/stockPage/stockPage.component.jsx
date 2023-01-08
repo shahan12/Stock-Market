38761aceb658e71d34ee12c788459a20d6a3e72d
Shahan Khan <shahankhan485@gmail.com>
2023-01-08 14:54:44 +0530

    implemented fuzzy search

--- src/pages/stockPage/stockPage.component.jsx
+++ src/pages/stockPage/stockPage.component.jsx
@@ -8,29 +8,7 @@ import FuzzySearch from "fuzzy-search";
 import { useHistory } from "react-router-dom";
 function StockPage() {
   const history = useHistory();
-  const [stockTableData, setStockTableData] = useState([
-    "Symbol,Name,Sector,Validtill",
-    "OBEROIRLTY,Oberoi Realty Limited,,2023-01-05 08:46:46",
-    "GODREJCP,Godrej Consumer Products Ltd.,Consumer Goods,2023-01-05 08:46:46",
-    "HAVELLS,Havells India Ltd.,Consumer Goods,2023-01-05 08:46:46",
-    "KSCL,Kaveri Seed Company Ltd.,Consumer Goods,2023-01-05 08:46:46",
-    "BRITANNIA,Britannia Industries Ltd.,Consumer Goods,2023-01-05 08:46:46",
-    "ZEEL,Zee Entertainment Enterprises Ltd.,Media & Entertainment,2023-01-05 08:46:46",
-    "LICHSGFIN,LIC Housing Finance Ltd.,Financial Services,2023-01-05 08:46:46",
-    "OIL,Oil India Ltd.,Energy,2023-01-05 08:46:46",
-    "TATACOMM,TATA COMMUNICATIONS,Telecom,2023-01-05 08:46:46",
-    "APOLLOTYRE,Apollo Tyres Ltd.,Automobile,2023-01-05 08:46:46",
-    "GRASIM,Grasim Industries Ltd.,Cement & Cement Products,2023-01-05 08:46:46",
-    "DABUR,Dabur India Ltd.,Consumer Goods,2023-01-05 08:46:46",
-    "RELINFRA,Reliance Infrastructure Ltd.,Energy,2023-01-05 08:46:46",
-    "RPOWER,Reliance Power Ltd.,Energy,2023-01-05 08:46:46",
-    "NBCC,NBCC (India) Ltd.,Construction,2023-01-05 08:46:46",
-    "DRREDDY,Dr. Reddy's Laboratories Ltd.,Pharma,2023-01-05 08:46:46",
-    "HEXAWARE,Hexaware Technologies Ltd.,Information Technology,2023-01-05 08:46:46",
-    "AMBUJACEM,Ambuja Cements Ltd.,Cement & Cement Products,2023-01-05 08:46:46",
-    "COALINDIA,Coal India Ltd.,Metals,2023-01-05 08:46:46",
-    "IFCI,IFCI Ltd.,Financial Services,2023-01-05 08:46:46",
-  ]);
+  const [stockTableData, setStockTableData] = useState([]);
   const [tableHeader, setTableHeader] = useState([]);
   const [sort, setSortIcon] = useState({ asc: false, dsc: false });
 
@@ -78,37 +56,14 @@ function StockPage() {
     const initiateSearch = new FuzzySearch(convertedData, ["Symbol", "Name"]);
     setStockTableData(initiateSearch.search(e.target.value));
   };
-  let convertedData = massageData([
-    "Symbol,Name,Sector,Validtill",
-    "OBEROIRLTY,Oberoi Realty Limited,,2023-01-05 08:46:46",
-    "GODREJCP,Godrej Consumer Products Ltd.,Consumer Goods,2023-01-05 08:46:46",
-    "HAVELLS,Havells India Ltd.,Consumer Goods,2023-01-05 08:46:46",
-    "KSCL,Kaveri Seed Company Ltd.,Consumer Goods,2023-01-05 08:46:46",
-    "BRITANNIA,Britannia Industries Ltd.,Consumer Goods,2023-01-05 08:46:46",
-    "ZEEL,Zee Entertainment Enterprises Ltd.,Media & Entertainment,2023-01-05 08:46:46",
-    "LICHSGFIN,LIC Housing Finance Ltd.,Financial Services,2023-01-05 08:46:46",
-    "OIL,Oil India Ltd.,Energy,2023-01-05 08:46:46",
-    "TATACOMM,TATA COMMUNICATIONS,Telecom,2023-01-05 08:46:46",
-    "APOLLOTYRE,Apollo Tyres Ltd.,Automobile,2023-01-05 08:46:46",
-    "GRASIM,Grasim Industries Ltd.,Cement & Cement Products,2023-01-05 08:46:46",
-    "DABUR,Dabur India Ltd.,Consumer Goods,2023-01-05 08:46:46",
-    "RELINFRA,Reliance Infrastructure Ltd.,Energy,2023-01-05 08:46:46",
-    "RPOWER,Reliance Power Ltd.,Energy,2023-01-05 08:46:46",
-    "NBCC,NBCC (India) Ltd.,Construction,2023-01-05 08:46:46",
-    "DRREDDY,Dr. Reddy's Laboratories Ltd.,Pharma,2023-01-05 08:46:46",
-    "HEXAWARE,Hexaware Technologies Ltd.,Information Technology,2023-01-05 08:46:46",
-    "AMBUJACEM,Ambuja Cements Ltd.,Cement & Cement Products,2023-01-05 08:46:46",
-    "COALINDIA,Coal India Ltd.,Metals,2023-01-05 08:46:46",
-    "IFCI,IFCI Ltd.,Financial Services,2023-01-05 08:46:46",
-  ]);
 
   /**
    * Called upon page load to get data from API
    */
   useEffect(() => {
-    setStockTableData(convertedData);
-    // getSockPageData()
+    getSockPageData();
   }, []);
+
   return (
     <article className={classes.container}>
       <div className={classes.pageName}>STOCK TABLE</div>
